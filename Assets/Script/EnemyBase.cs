ef11c85535c641b7815318a222af91ad70bf33fb
HoangQuocKhai <hoangquockhai2505@gmail.com>
2026-02-04 22:41:43 +0700

    update ani enemy

---
+++ Assets/Script/EnemyBase.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    [Header("Stats")]
+    public float maxHP = 100f;
+    protected float currentHP;
+
+    [Header("Movement")]
+    public float moveSpeed = 2f;
+    protected NavMeshAgent agent;
+    protected Transform player;
+
+    [Header("Attack (Common)")]
+    public float attackRange = 2f;
+    public float attackCooldown = 1.5f;
+    protected float lastAttackTime;
+
+    protected virtual void Start()
+    {
+        currentHP = maxHP;
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = moveSpeed;
+        agent.stoppingDistance = attackRange;
+        GameObject p = GameObject.FindGameObjectWithTag("Player");
+        if (p != null)
+            player = p.transform;
+    }
+
+    protected virtual void Update()
+    {
+        if (player == null) return;
+
+        agent.SetDestination(player.position);
+
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance <= attackRange && Time.time >= lastAttackTime + attackCooldown)
+        {
+            lastAttackTime = Time.time;
+            Attack();
+        }
+    }
+
+    //mỗi enemy sẽ đánh theo cách riêng
+    protected abstract void Attack();
+
+    public virtual void TakeDamage(float damage)
+    {
+        currentHP -= damage;
+        if (currentHP <= 0)
+            Die();
+    }
+
+    protected virtual void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Bullet"))
+    //    {
+    //        TakeDamage(20f);
+    //        Destroy(other.gameObject);
+    //    }
+    //}
+}
---
+++ Assets/Script/Enemyfly.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class BookFloatAnim : MonoBehaviour
+{
+    public float floatHeight = 0.3f;
+    public float floatSpeed = 2f;
+    public float swayAngle = 10f;
+    public float swaySpeed = 2f;
+
+    Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        // Lơ lửng lên xuống
+        float yOffset = Mathf.Sin(Time.time * floatSpeed) * floatHeight;
+        transform.localPosition = startPos + new Vector3(0, yOffset, 0);
+
+        // Lắc lư
+        float zRot = Mathf.Sin(Time.time * swaySpeed) * swayAngle;
+        transform.localRotation = Quaternion.Euler(0, 0, zRot);
+    }
+}
